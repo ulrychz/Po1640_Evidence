88f72431575de0de599dbd083900323860b57ace
Zdenek ULRYCH <ulrychz@office365.zcu.cz>
2024-03-18 18:23:14 +0100

    cviceni 6

--- Po1640_Evidence/Models/Evidence.cs
+++ Po1640_Evidence/Models/Evidence.cs
@@ -1,10 +1,21 @@
-﻿namespace Po1640_Evidence.Models
+﻿using Microsoft.JSInterop;
+using System.Globalization;
+
+namespace Po1640_Evidence.Models
 {
     public class Evidence
     {
+        public Evidence(IJSRuntime js) 
+        { 
+            JS = js;
+            //CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("en-US");
+            CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("cs-CZ");
+        }
+        private IJSRuntime JS { get; set; }
         public List<Polozka> Polozky { get; set; } = new List<Polozka>();
         public Polozka Polozka { get; set; } = new Polozka();
         public string Vystup { get; set; } = "";
+        public bool IsEditace { get; set; } = false;
         public void PridatZaznam()
         {
             //Polozky.Add(Polozka);
@@ -20,5 +31,60 @@
             Vystup = $"Jednotlivé záznamy jsou:<br> {string.Join("<br>", Polozky)}";
 
         }
+        public void Statistiky()
+        {
+            Vystup = "";
+            Vystup += $"Minimum: {Minimum()} <br>";
+            Vystup += $"Maximum: {Maximum()} <br>";
+            Vystup += $"Průměr: {Prumer()}";
+        }
+
+        private double Minimum()
+        {
+            if (Polozky.Count == 0)
+            {
+                return double.NaN;
+            }
+            return Polozky.Min(pol=>pol.Zisk);
+        }
+
+        private double Maximum()
+        {
+            if (Polozky.Count == 0)
+            {
+                return double.NaN;
+            }
+            return Polozky.Max(pol => pol.Zisk);
+        }
+        private double Prumer()
+        {
+            if (Polozky.Count == 0)
+            {
+                return double.NaN;
+            }
+            return Polozky.Average(pol => pol.Zisk);
+        }
+
+        public async Task SmazatZaznam(Models.Polozka item)
+        {
+            var zprava = $"Chcete smazat {item.Datum} - Zisk: {item.Zisk} z vašeho seznamu?";
+            //JS.InvokeVoidAsync("alert", zprava);
+            if (await JS.InvokeAsync<bool>("confirm", zprava))
+            {
+                Polozky.Remove(item);
+            }
+        }
+        public void Editace(Polozka item)
+        {
+            Polozka = item;
+            IsEditace = true;
+        }
+        public void UkonciEditaci()
+        {
+            IsEditace = false;
+            Polozka = new Polozka();
+        }
     }
+
+   
 }
